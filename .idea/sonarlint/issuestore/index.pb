7bfa3d9fab91150eae942b5326a44e0be92f1373
Maikel Chandika <mchandika@bbmtek.com>
2020-01-08 07:09:11 +0700

    wip

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+;
+src/main.rs,4/b/4b569f42a6967dec04275af54f4ca9ab6a4eee64
+:
+
+src/lib.rs,b/2/b24749917179fb5e3e613ed2a703fcdcc6cdf9da
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+
+use mkgrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = mkgrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
